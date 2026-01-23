ef23f284f3eb7d3125f3dec67c0cbb2088043ca2
Dict_Bokokugo <yuudai.klt@gmail.com>
2026-01-23 11:14:42 +0900

    DebugCamera

--- project/Dict_Engine/io/DebugCamera.cpp
+++ project/Dict_Engine/io/DebugCamera.cpp
@@ -1,6 +1,6 @@
 #include "DebugCamera.h"
 #include "InputManager.h"
-#include "imgui.h"
+#include "ImGuiManager.h"
 
 void DebugCamera::Initialize()
 {
@@ -17,7 +17,7 @@ void DebugCamera::Update(InputManager* input, Transform originCamera)
 
 	Transform camera = { 0.0f, 0.0f, 0.0f };
 
-	if (input_->PushKey(DIK_RIGHT))
+	/*if (input_->PushKey(DIK_RIGHT))
 	{
 		debugCamera_.rotate.y += 0.5f / 180.0f * pi;
 	}
@@ -70,9 +70,49 @@ void DebugCamera::Update(InputManager* input, Transform originCamera)
 	{
 		const float speed = -0.5f;
 		move.y = speed;
+	}*/
+
+	ImGui::Begin("DebugCameraWindow");
+
+	if (input->PushMouse(2) && !ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow))
+	{
+		// ウィンドウ上ではない領域でのドラッグ
+		ImVec2 delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Middle, 0.0f);
+
+		// カメラ角度に反映
+		phi += delta.x * rotationSpeed;
+		theta += delta.y * rotationSpeed;
+
+		if (phi > pi)
+		{
+			phi = phi - 2.0f * pi;
+		} else if (phi < -pi)
+		{
+			phi = phi + 2.0f * pi;
+		}
+
+		// ピッチを上下90°未満にクランプ
+		const float limit = 0.4999f * pi; // 約85°
+		theta = std::clamp(theta, -limit, limit);
+
+		ImGui::ResetMouseDragDelta(ImGuiMouseButton_Middle);
+
+		ImGui::Text("Delta x : %3.6f, y : %3.6f", delta.x, delta.y);
+
 	}
 
-	Matrix4x4 rotMat = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, camera.rotate, { 0.0f, 0.0f, 0.0f });
+	LONG wheel = input->MouseWheel(); // 前フレームとの差分
+	radius -= wheel * 0.005f;              // 感度は 0.1f などで調整
+	radius = max(1.0f, min(radius, 20.0f)); // クランプして近づきすぎ防止
+
+	camera.rotate.x = phi;
+	camera.rotate.y = theta;
+
+	camera.translate.x = radius * std::cos(theta) * std::sin(phi);
+	camera.translate.y = radius * std::sin(theta);
+	camera.translate.z = radius * std::cos(theta) * std::cos(phi);
+
+	/*Matrix4x4 rotMat = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, camera.rotate, { 0.0f, 0.0f, 0.0f });
 
 	Vector3 rotatedMove =
 	{
@@ -83,11 +123,15 @@ void DebugCamera::Update(InputManager* input, Transform originCamera)
 
 	debugCamera_.translate.x += rotatedMove.x;
 	debugCamera_.translate.y += rotatedMove.y;
-	debugCamera_.translate.z += rotatedMove.z;
-	
-	camera.translate.x = debugCamera_.translate.x + originCamera.translate.x;
-	camera.translate.y = debugCamera_.translate.y + originCamera.translate.y;
-	camera.translate.z = debugCamera_.translate.z + originCamera.translate.z;
+	debugCamera_.translate.z += rotatedMove.z;*/
 
-	viewMatrix_ = Inverse(MakeAffineMatrix(originCamera.scale, camera.rotate, camera.translate));
+	/*camera.translate.x = debugCamera_.translate.x + originCamera.translate.x;
+	camera.translate.y = debugCamera_.translate.y + originCamera.translate.y;
+	camera.translate.z = debugCamera_.translate.z + originCamera.translate.z;*/
+
+	/*worldMatrix_ = MakeAffineMatrix(originCamera.scale, camera.rotate, camera.translate);*/
+
+	viewMatrix_ = MakeLookAtMatrix(camera.translate, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f });
+
+	ImGui::End();
 }
